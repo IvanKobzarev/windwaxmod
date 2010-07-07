4e9087f75afa6b6abd7f6a74bdabd1517a226578
berjebobik <berjebobik@c9efe5ff-e2e0-7e4f-8bce-482370ba6f95>
2010-07-07 09:20:31 +0000

    -

--- trunk/ProfileBallone/ProfileBallone.cpp
+++ trunk/ProfileBallone/ProfileBallone.cpp
@@ -27,10 +27,11 @@
 GLUI *glui;
 GLUI_StaticText *FicProject;
 TAxe *AxeProfiles;
-
+TAxe *AxeSel;
+Courbe * CourbProfile;
 Forme *F;
 char ProjectName[255];
-
+int FenetreProfiles;
 /*divers tableaux*/
 Matrice *IntProfCent, *ExtProfCent, *IntProfBout, *ExtProfBout;
 Matrice** ReperPoints;
@@ -41,6 +42,13 @@ char NomFichierVentHoles[255];
 char NomFichierDiagNerv[255];
 
 WindPatternsProject* gfd = new WindPatternsProject();
+GLUI_Spinner *SpinNoNerv[2];
+int NoNerv[2] = {2, 3};
+float dyw=2, wN=25, kChord=1.05, kMf=1.0;
+int xSouris, ySouris;
+int zoomIN = false, debZoomIN = false, finZoomIN = false, zoomOUT = false, quitZoom = false;
+
+Courbe *CourbZoom;
 
 void display(void) {
     //CalculVue3dEtPatron();
@@ -61,6 +69,87 @@ void keyboard(unsigned char key, int x, int y) {
 }
 
 void BoutonSouris(int button, int state, int x, int y) {
+    int Fenetre;
+    xSouris = x;
+    ySouris = y;
+    AxeSel = NULL;
+
+    Fenetre = glutGetWindow();
+
+	if (Fenetre == FenetreProfiles) {
+        AxeSel = AxeProfiles;
+    }
+
+    /*test debut/fin, rotation/translation/zoom pour Axe3d*/
+    if (AxeSel == AxeProfiles) {
+        if (state == GLUT_DOWN) {
+            switch (button) {
+                case GLUT_LEFT_BUTTON: //zoomIN
+                    if (zoomOUT) {
+                        quitZoom = true;
+                        break;
+                    }
+                    //test si debut de zoom
+                    if (!zoomIN) {
+                        zoomIN = true, debZoomIN = true;
+                    }
+                    CourbZoom->segments = ON;
+                    AxeProfiles->XAuto = OFF;
+                    AxeProfiles->YAuto = OFF;
+                    AxeProfiles->ZAuto = OFF;
+                    break;
+                case GLUT_RIGHT_BUTTON: //zoomOUT x2
+
+                    if (zoomIN) {
+                        quitZoom = true;
+                        break;
+                    }
+
+                    zoomOUT = true;
+
+                    AxeProfiles->XAuto = OFF;
+                    AxeProfiles->YAuto = OFF;
+                    AxeProfiles->ZAuto = OFF;
+
+                    break;
+
+                case GLUT_MIDDLE_BUTTON:
+
+                    quitZoom = true;
+
+                    break;
+
+                default: printf("\n type de bouton souris inconnu !!!");
+
+            }
+
+        } else /*state = GLUT_UP*/ {
+            if (zoomIN) {
+                zoomIN = false;
+                finZoomIN = true;
+                CourbZoom->segments = OFF;
+                motion(x, y); //appel a motion pour maj axe
+
+            } else if (zoomOUT) {
+                motion(x, y); //appel a motion pour maj axe
+            } else if (quitZoom) {
+                quitZoom = false;
+                zoomIN = false;
+                debZoomIN = false;
+                finZoomIN = false;
+                zoomOUT = false;
+
+                AxeProfiles->XAuto = ON;
+                AxeProfiles->YAuto = ON;
+                AxeProfiles->ZAuto = ON;
+            }
+
+            glutPostRedisplay();
+
+        }
+
+    }
+
 }
 
 void InitFenetre(void) {
@@ -130,16 +219,59 @@ void ChargerFichierProject(int /*control*/) {
     glui->sync_live();
 }
 
+ProfilGeom* getProfilGeom(Forme* f, int nerv) {
+	ProfilGeom* pg = new ProfilGeom();
+	
+	return pg;
+}
+
+void Apply(int /*control*/) {
+    AxeProfiles->XAuto = ON;
+    AxeProfiles->YAuto = ON;
+    AxeProfiles->ZAuto = ON;
+    AxeProfiles->xmin = -2.0;
+    AxeProfiles->xmax = 2.0;
+    AxeProfiles->ymin = -2.0;
+    AxeProfiles->ymax = 2.0;
+
+	AxeProfiles->XGrid == OFF;
+	AxeProfiles->YGrid == OFF;
+	AxeProfiles->ZGrid == OFF;
+
+    LibererCourbesAxe(AxeProfiles);
+
+	ProfilGeom pg = getProfilGeom(F, NoNerv[0]);
+
+    CourbProfile = new Courbe("Profile");
+    CourbProfile->points = OFF;
+    CourbProfile->symX = OFF;
+    CourbProfile->pts = new Matrice(4, 2);
+	CourbProfile->pts->SetElement(0,0,0);
+	CourbProfile->pts->SetElement(0,1,0);
+
+	CourbProfile->pts->SetElement(1,0,1.0);
+	CourbProfile->pts->SetElement(1,1,0.0);
+
+	CourbProfile->pts->SetElement(2,0,1.0);
+	CourbProfile->pts->SetElement(2,1,0.5);
+
+	CourbProfile->pts->SetElement(3,0,0.5);
+	CourbProfile->pts->SetElement(3,1,0.5);
+
+	AjoutCourbe(AxeProfiles, CourbProfile);
+	display();
+}
+
 int main(int argc, char** argv)
 {
-	printf ("\nProfile ballone");
+	printf ("Profile ballone...");
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(500, 500);
     glutInitWindowPosition(100, 100);
 
-	int FenetreProfiles = glutCreateWindow("Profile view");
+	FenetreProfiles = glutCreateWindow("Profile view");
     InitFenetre(); 
 
     AxeProfiles = CreerAxe(FenetreProfiles);
@@ -158,17 +290,50 @@ int main(int argc, char** argv)
             glui->add_button("Load project", 0, &ChargerFichierProject);
     btnLoadForme->set_w(10);
 
+	GLUI_Panel *panel1 = glui->add_panel("");
+
+	SpinNoNerv[0] = glui->add_spinner_to_panel(panel1, "No Nerv 1", GLUI_SPINNER_INT, &(NoNerv[0]));
+    //SpinNoNerv[0] -> set_int_limits(-1, F->m_nbProfils - 1);
+
+	SpinNoNerv[1] = glui->add_spinner_to_panel(panel1, "No Nerv 2", GLUI_SPINNER_INT, &(NoNerv[1]));
+    //SpinNoNerv[1] -> set_int_limits(-1, F->m_nbProfils - 1);
+
+    GLUI_Button *btnApply =
+            glui->add_button("Apply", 0, &Apply);
+    btnApply->set_w(10);
+
     glui->add_column(true);
 
 	strcpy(NomFichierProject, "f17project.wpp");
 
+	gfd = LectureWindPatternsProject(NomFichierProject);
+    LoadFromWindPatternsProject(gfd);
+
     FicProject = glui->add_statictext("???");
     FicProject->set_text(NomFichierProject);
 
-    GLUI_Panel *panel = glui->add_panel("");
+	GLUI_Panel *panel2 = glui->add_panel("");
+	
+    GLUI_Spinner *SpinKchord = glui->add_spinner_to_panel(
+            panel2, "K chord", GLUI_SPINNER_FLOAT, &(kChord));
+    SpinKchord -> set_float_limits(0.8, 1.5);
+
+    GLUI_Spinner *SpinKmf = glui->add_spinner_to_panel(
+            panel2, "K move forward", GLUI_SPINNER_FLOAT, &(kMf));
+    SpinKmf -> set_float_limits(0.0, 1.0);
+
+    GLUI_Spinner *SpinWn = glui->add_spinner_to_panel(
+            panel2, "W new", GLUI_SPINNER_FLOAT, &(wN));
+    SpinWn -> set_float_limits(0.0, 1.0);
+
+    GLUI_Spinner *SpinDyw = glui->add_spinner_to_panel(
+            panel2, "D yw", GLUI_SPINNER_FLOAT, &(dyw));
+    SpinDyw -> set_float_limits(0.0, 1.0);
+
+    //GLUI_Panel *panel = glui->add_panel("");
 
     GLUI_Master.set_glutIdleFunc(NULL);
-
+	Apply();
     glui->sync_live();
 
     display();
