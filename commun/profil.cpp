068bd1eec9d707c2abac87bd797e22a540f60ee1
berjebobik <berjebobik@c9efe5ff-e2e0-7e4f-8bce-482370ba6f95>
2010-08-02 21:57:10 +0000

    -

--- commun/profil.cpp
+++ commun/profil.cpp
@@ -63,6 +63,57 @@ void ProfilGeom::print() {
 	}
 }
 
+/*ProfilGeom* getHvostByPince(ProfilGeom* pg0, double posF, double pincePower, double Amp, int* ie, int* ii) {
+	ProfilGeom* pg = new ProfilGeom();
+	int ne = pg0->ExtProf->GetLignes();
+	int ni = pg0->IntProf->GetLignes();
+	pg -> ExtProf = new Matrice (ne, 2);
+	pg -> IntProf = new Matrice (ni, 2);
+
+	
+
+	for (int i = 0; i < ne; i++) {
+		double xi0 = pg0 -> ExtProf->Element(i, 0);
+		double yi0 = pg0 -> ExtProf->Element(i, 1);
+		if (xi0 >= posF) {
+			double xi = x0_ + xi0 * l_/l;
+			double yi = y0_ + yi0 * wN/w0;
+			pg-> ExtProf->SetElement(i, 0, xi);
+			pg-> ExtProf->SetElement(i, 1, yi);
+		} else {
+			pg-> ExtProf->SetElement(i, 0, xi0);
+			pg-> ExtProf->SetElement(i, 1, yi0);
+		}
+	}
+	for (int i = 0; i < ni; i++) {
+		double xi0 = pg0 -> IntProf->Element(i, 0);
+		double yi0 = pg0 -> IntProf->Element(i, 1);
+		if (xi0 == posF) xi0F = xi0;
+		if (xi0 >= posF) {
+			
+			double xi = x0_ + xi0 * l_/l;
+			double yi = y0_ + yi0 * wN/w0;
+
+			// here need to use pince function
+			double amp = Amp * functionPincePower((IntProf->Element(ni - 1, 0) - xi)/(xi0F-xi), (1.0f/pincePower));
+
+
+
+			pg-> IntProf->SetElement(i, 0, xi);
+			pg-> IntProf->SetElement(i, 1, yi);
+		} else {
+			pg-> IntProf->SetElement(i, 0, xi0);
+			pg-> IntProf->SetElement(i, 1, yi0);
+		}
+	}
+	
+
+	
+
+	return pg;
+}
+*/
+
 ProfilGeom* getBalloneProfilGeom(ProfilGeom* pg0, double kChord, double kMf, double w0, double wN, double dyw) {
 	ProfilGeom* pg = new ProfilGeom();
 
@@ -74,11 +125,11 @@ ProfilGeom* getBalloneProfilGeom(ProfilGeom* pg0, double kChord, double kMf, dou
 	double l = abs (pg0->ExtProf->Element(ne-1, 0) - pg0->ExtProf->Element(0, 0));
 
 	double l_ = l * kChord;
-	double dl = l * (kChord-1.0);
+	double dl = l * (kChord - 1.0);
 
 
-	double x0_ = pg0->ExtProf->Element(0, 0) - dl * kMf; 
-	double wabs0 = EpaisseurRelative(pg0->ExtProf, pg0->IntProf);
+	double x0_ = pg0 -> ExtProf -> Element(0, 0) - dl * kMf; 
+	double wabs0 = EpaisseurRelative(pg0 -> ExtProf, pg0 -> IntProf);
 	double y0_ = wabs0 * dyw/w0;
 
 	for (int i = 0; i < ne; i++) {
@@ -103,4 +154,66 @@ ProfilGeom* getBalloneProfilGeom(ProfilGeom* pg0, double kChord, double kMf, dou
 	}
 
 	return pg;
+}
+
+ProfilGeom* getProfilGeomTailDown(ProfilGeom* pg1, double xv) {
+	ProfilGeom* pg1h = new ProfilGeom();
+
+	int ne = pg1->ExtProf->GetLignes();
+	int ni = pg1->IntProf->GetLignes();
+	
+	pg1h -> ExtProf = new Matrice (ne, 2);
+	pg1h -> IntProf = new Matrice (ni, 2);
+
+	double dw = pg1 -> ExtProf -> Element (ne-1, 1);
+	printf ("\n dw=%f", dw);
+	bool found = false;
+	double xf = 0.0, yf = 0.0;
+
+	for (int i = 0; i < ne; i++) {
+		double xi0 = pg1 -> ExtProf->Element(i, 0);
+		double yi0 = pg1 -> ExtProf->Element(i, 1);
+		
+		if (found) {
+			double xi = xi0;
+			double yi = (yi0-dw)*yf/(yf-dw);
+			pg1h -> ExtProf->SetElement(i, 0, xi);
+			pg1h -> ExtProf->SetElement(i, 1, yi);
+		} else {
+			pg1h -> ExtProf->SetElement(i, 0, xi0);
+			pg1h -> ExtProf->SetElement(i, 1, yi0);
+		}
+
+		if (xi0 >= xv) {
+			found = true;
+			xf = xi0;
+			yf = yi0;
+		}
+	}
+	found = false;
+	for (int i = 0; i < ni; i++) {
+		double xi0 = pg1 -> IntProf->Element(i, 0);
+		double yi0 = pg1 -> IntProf->Element(i, 1);
+
+		
+		if (found) {
+			double xi = xi0;
+			double yi = yf*(dw-yi0)/(dw + abs(yf));
+			pg1h -> IntProf->SetElement(i, 0, xi);
+			pg1h -> IntProf->SetElement(i, 1, yi);
+		} else {
+			pg1h -> IntProf->SetElement(i, 0, xi0);
+			pg1h -> IntProf->SetElement(i, 1, yi0);
+		}
+
+
+		if (xi0 >= xv) {
+			found = true;
+			xf = xi0;
+			yf = yi0;
+		}
+
+	}
+
+	return pg1h;
 }
