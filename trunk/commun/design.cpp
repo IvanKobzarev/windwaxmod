b6499460e2c0ace7eb04112b53b65e035d4e3cf7
berjebobik <berjebobik@c9efe5ff-e2e0-7e4f-8bce-482370ba6f95>
2011-04-04 22:04:18 +0000

    - design.h design.cpp added

---
+++ trunk/commun/design.cpp
@@ -0,0 +1,19 @@
+//design.cpp
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <math.h>
+#include <fstream>
+#include <iomanip>
+
+#include "fichier.h"
+#include "profil.h"
+#include "geom.h"
+
+int test() {
+	printf ("\n test()");
+	return 0;
+}
